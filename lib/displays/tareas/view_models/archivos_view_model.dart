9b2097ad446d7dbeb7f21243f8606084f4318cc1
Karla Catu <kcatu@demosoft.com.gt>
2024-04-01 10:43:11 -0600

    copiar codigo y separarlo por view y view model, hay errro al abrir el explorador.

--- lib/displays/tareas/view_models/archivos_view_model.dart
+++ lib/displays/tareas/view_models/archivos_view_model.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ArchivosViewModel extends ChangeNotifier {
   //manejar flujo del procesp
@@ -12,7 +15,46 @@ class ArchivosViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  abrirExporador(BuildContext context) {
+  // Función para abrir el explorador de archivos
+  void abrirExplorador(BuildContext context) {
     print("Abrir explorador de archivos.");
+    // Aquí puedes agregar la lógica para abrir el explorador de archivos
+  }
+
+  // Función para seleccionar imágenes
+  Future<void> selectImages(
+    BuildContext context,
+    Function(List<XFile>?) onSelectImages,
+  ) async {
+    try {
+      isLoading = true; // Indicar que se está cargando
+      final ImagePicker picker = ImagePicker();
+      final List<XFile> pickedFiles = await picker.pickMultiImage();
+      onSelectImages(pickedFiles);
+    } catch (e) {
+      // Manejar errores aquí
+      print('Error selecting images: $e');
+    } finally {
+      isLoading = false; // Indicar que se ha completado la carga
+    }
+  }
+
+  List<File> selectedImages = [];
+  List<File> selectedVideos = [];
+
+  void handleImageSelection(List<XFile>? selectedFiles) {
+    if (selectedFiles != null) {
+      print(selectedFiles.length);
+
+      selectedImages = selectedFiles.map((xFile) => File(xFile.path)).toList();
+      notifyListeners();
+    }
+  }
+
+  void handleVideoSelection(XFile? selectedFile) {
+    if (selectedFile != null) {
+      selectedVideos.add(File(selectedFile.path));
+      notifyListeners();
+    }
   }
 }
--- lib/displays/tareas/views/archivos_view.dart
+++ lib/displays/tareas/views/archivos_view.dart
@@ -1,13 +1,20 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_post_printer_example/displays/tareas/view_models/view_models.dart';
 import 'package:flutter_post_printer_example/themes/app_theme.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class ArchivosView extends StatelessWidget {
   const ArchivosView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final vm = Provider.of<ArchivosViewModel>(context);
+
     return Stack(
       children: [
         Scaffold(
@@ -17,18 +24,83 @@ class ArchivosView extends StatelessWidget {
               style: AppTheme.titleStyle,
             ),
           ),
-          body: Center(
-            child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.attach_file_outlined,
-                size: 50,
-              ),
-              tooltip: "Adjuntar Archivos",
-            ),
+          body: Column(
+            children: [
+              ImageSelector(onSelectImages: vm.handleImageSelection),
+              if (vm.selectedImages.isNotEmpty)
+                ImagePreview(images: vm.selectedImages),
+              // Center(
+              //   child: IconButton(
+              //     onPressed: () => vm.abrirExplorador(context),
+              //     icon: const Icon(
+              //       Icons.attach_file_outlined,
+              //       size: 50,
+              //     ),
+              //     tooltip: "Adjuntar Archivos",
+              //   ),
+              // ),
+              // FloatingActionButton(
+              //   onPressed: () {
+              //     vm.selectImages(context, (List<XFile>? pickedFiles) {
+              //       // Aquí puedes manejar las imágenes seleccionadas
+              //     });
+              //   },
+              //   heroTag: 'imageSelector',
+              //   tooltip: 'Pick Images from gallery',
+              //   child: const Icon(Icons.photo),
+              // )
+            ],
           ),
         )
       ],
+    );
+  }
+}
+
+class ImageSelector extends StatelessWidget {
+  final Function(List<XFile>?) onSelectImages;
+
+  const ImageSelector({
+    super.key,
+    required this.onSelectImages,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = Provider.of<ArchivosViewModel>(context);
+
+    return FloatingActionButton(
+      onPressed: () {
+        vm.selectImages(context, (List<XFile>? pickedFiles) {
+          // Aquí puedes manejar las imágenes seleccionadas
+        });
+      },
+      heroTag: 'imageSelector',
+      tooltip: 'Pick Images from gallery',
+      child: const Icon(
+        Icons.photo,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class ImagePreview extends StatelessWidget {
+  final List<File> images;
+
+  const ImagePreview({
+    super.key,
+    required this.images,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, index) {
+        print(images.length);
+        return Image.file(images[index]);
+      },
     );
   }
 }
