93e7ed89b7ae6690326acc48b06f46f6abd49967
Karla Catu <kcatu@demosoft.com.gt>
2024-05-17 10:19:48 -0600

    traduccion aplicada ar archivo types_doc_view

--- lib/displays/listado_Documento_Pendiente_Convertir/views/types_doc_view.dart
+++ lib/displays/listado_Documento_Pendiente_Convertir/views/types_doc_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_post_printer_example/displays/listado_Documento_Pendiente_Convertir/models/models.dart';
 import 'package:flutter_post_printer_example/displays/listado_Documento_Pendiente_Convertir/view_models/view_models.dart';
+import 'package:flutter_post_printer_example/services/services.dart';
 import 'package:flutter_post_printer_example/themes/app_theme.dart';
+import 'package:flutter_post_printer_example/utilities/translate_block_utilities.dart';
 import 'package:flutter_post_printer_example/view_models/view_models.dart';
 import 'package:flutter_post_printer_example/widgets/widgets.dart';
 import 'package:provider/provider.dart';
@@ -17,55 +19,59 @@ class TypesDocView extends StatelessWidget {
     return Stack(
       children: [
         Scaffold(
-            appBar: AppBar(
-              title: Text(
-                vmMenu.name,
-                style: AppTheme.titleStyle,
-              ),
+          appBar: AppBar(
+            title: Text(
+              vmMenu.name,
+              style: AppTheme.titleStyle,
             ),
-            body: RefreshIndicator(
-              onRefresh: () => vm.loadData(context),
-              child: ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              "Registros(${vm.documents.length})",
-                              style: AppTheme.normalBoldStyle,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        ListView.builder(
-                          physics: const NeverScrollableScrollPhysics(),
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: vm.documents.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            final TypeDocModel doc = vm.documents[index];
-                            return CardWidget(
-                              child: ListTile(
-                                onTap: () => vm.navigatePendDocs(context, doc),
-                                trailing: const Icon(Icons.arrow_right),
-                                title: Text(
-                                  "${doc.fDesTipoDocumento} (${doc.tipoDocumento})",
-                                  style: AppTheme.normalStyle,
-                                ),
+          ),
+          body: RefreshIndicator(
+            onRefresh: () => vm.loadData(context),
+            child: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "${AppLocalizations.of(context)!.translate(
+                              BlockTranslate.general,
+                              'registro',
+                            )} (${vm.documents.length})",
+                            style: AppTheme.normalBoldStyle,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: vm.documents.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final TypeDocModel doc = vm.documents[index];
+                          return CardWidget(
+                            child: ListTile(
+                              onTap: () => vm.navigatePendDocs(context, doc),
+                              trailing: const Icon(Icons.arrow_right),
+                              title: Text(
+                                "${doc.fDesTipoDocumento} (${doc.tipoDocumento})",
+                                style: AppTheme.normalStyle,
                               ),
-                            );
-                          },
-                        ),
-                      ],
-                    ),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            )),
+                ),
+              ],
+            ),
+          ),
+        ),
         if (vm.isLoading)
           ModalBarrier(
             dismissible: false,
