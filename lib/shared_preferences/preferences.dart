70776aa6169721c6a2eea60eb7306205d9adbfe5
Karla Catu <kcatu@demosoft.com.gt>
2024-05-31 12:44:04 -0600

    creacion de las prefercias a utilizar

--- lib/shared_preferences/preferences.dart
+++ lib/shared_preferences/preferences.dart
@@ -14,6 +14,8 @@ class Preferences {
   static const String _docKey = "docKey";
   static const String _langKey = "langKey";
   static const String _idLangKey = "idLangKey";
+  static const String _themeKey = "theme";
+  static const String _themeNameKey = "nameTheme";
 
   //iniciar shared preferences
   static Future init() async {
@@ -104,5 +106,22 @@ class Preferences {
 
   static set idLanguage(int value) {
     _prefs.setInt(_idLangKey, value);
+  }
+
+  //ID del tema de la aplicacion
+  static int get theme {
+    return _prefs.getInt(_themeKey) ?? 0;
+  }
+
+  static set theme(int value) {
+    _prefs.setInt(_themeKey, value);
+  }
+
+  static String get themeName {
+    return _prefs.getString(_themeNameKey) ?? "";
+  }
+
+  static set themeName(String value) {
+    _prefs.setString(_themeNameKey, value);
   }
 }
