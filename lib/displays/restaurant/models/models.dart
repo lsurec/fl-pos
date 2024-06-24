587625f45575eb509328d3752807230053d2b5bc
Robert Morales <lsurec@demosoft.com.gt>
2024-06-24 11:41:15 -0600

    "Added getLocations method to RestaurantService class with API call and response handling"

---
+++ lib/displays/restaurant/models/models.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_post_printer_example/displays/restaurant/models/locations_model.dart';
--- lib/displays/restaurant/services/restaurant_service.dart
+++ lib/displays/restaurant/services/restaurant_service.dart
@@ -1,8 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter_post_printer_example/displays/restaurant/models/models.dart';
+import 'package:flutter_post_printer_example/models/models.dart';
 import 'package:flutter_post_printer_example/shared_preferences/preferences.dart';
 import 'package:http/http.dart' as http;
 
 class RestaurantService {
   final String _baseUrl = Preferences.urlApi;
 
-  getLocation() {}
+  Future<ApiResModel> getLocations(
+    int typeDoc,
+    int enterprise,
+    int station,
+    String series,
+    String user,
+    String token,
+  ) async {
+    //url completa
+    Uri url = Uri.parse("${_baseUrl}Restaurant/locations");
+
+    try {
+      //Configuraciones del api
+      final response = await http.get(
+        url,
+        headers: {
+          "Authorization": "bearer $token",
+          "typeDoc": "$typeDoc",
+          "enterprise": "$enterprise",
+          "station": "$station",
+          "series": series,
+          "user": user,
+        },
+      );
+
+      ResponseModel res = ResponseModel.fromMap(jsonDecode(response.body));
+
+      //si el api no responde
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        return ApiResModel(
+          url: url.toString(),
+          succes: false,
+          response: res.data,
+          storeProcedure: res.storeProcedure,
+        );
+      }
+
+      //bodegas disponibles
+      List<LocationsModel> locations = [];
+
+      //recorrer lista api Y  agregar a lista local
+      for (var item in res.data) {
+        //Tipar a map
+        final responseFinally = LocationsModel.fromMap(item);
+        //agregar item a la lista
+        locations.add(responseFinally);
+      }
+
+      //respuesta correcta
+      return ApiResModel(
+        url: url.toString(),
+        succes: true,
+        response: locations,
+        storeProcedure: null,
+      );
+    } catch (e) {
+      //respuesta incorrecta
+      return ApiResModel(
+        url: url.toString(),
+        succes: false,
+        response: e.toString(),
+        storeProcedure: null,
+      );
+    }
+  }
 }
