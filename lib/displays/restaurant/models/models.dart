a13da6ceb6d6fe218e24daf1fccaf0c44527f13d
Robert Morales <lsurec@demosoft.com.gt>
2024-06-24 15:14:22 -0600

    "Added TableModel export to models.dart and implemented getTables method in RestaurantService.dart"

--- lib/displays/restaurant/models/models.dart
+++ lib/displays/restaurant/models/models.dart
@@ -1,1 +1,2 @@
 export 'package:flutter_post_printer_example/displays/restaurant/models/locations_model.dart';
+export 'package:flutter_post_printer_example/displays/restaurant/models/table_model.dart';
--- lib/displays/restaurant/services/restaurant_service.dart
+++ lib/displays/restaurant/services/restaurant_service.dart
@@ -8,6 +8,75 @@ import 'package:http/http.dart' as http;
 class RestaurantService {
   final String _baseUrl = Preferences.urlApi;
 
+  Future<ApiResModel> getTables(
+    int typeDoc,
+    int enterprise,
+    int station,
+    String series,
+    int elementAssigned,
+    String user,
+    String token,
+  ) async {
+    //url completa
+    Uri url = Uri.parse("${_baseUrl}Restaurant/tables");
+
+    try {
+      //Configuraciones del api
+      final response = await http.get(
+        url,
+        headers: {
+          "Authorization": "bearer $token",
+          "typeDoc": "$typeDoc",
+          "enterprise": "$enterprise",
+          "station": "$station",
+          "series": series,
+          "elementAssigned": "$elementAssigned",
+          "user": user,
+          "token": token,
+        },
+      );
+
+      ResponseModel res = ResponseModel.fromMap(jsonDecode(response.body));
+
+      //si el api no responde
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        return ApiResModel(
+          url: url.toString(),
+          succes: false,
+          response: res.data,
+          storeProcedure: res.storeProcedure,
+        );
+      }
+
+      //bodegas disponibles
+      List<TableModel> tables = [];
+
+      //recorrer lista api Y  agregar a lista local
+      for (var item in res.data) {
+        //Tipar a map
+        final responseFinally = TableModel.fromMap(item);
+        //agregar item a la lista
+        tables.add(responseFinally);
+      }
+
+      //respuesta correcta
+      return ApiResModel(
+        url: url.toString(),
+        succes: true,
+        response: tables,
+        storeProcedure: null,
+      );
+    } catch (e) {
+      //respuesta incorrecta
+      return ApiResModel(
+        url: url.toString(),
+        succes: false,
+        response: e.toString(),
+        storeProcedure: null,
+      );
+    }
+  }
+
   Future<ApiResModel> getLocations(
     int typeDoc,
     int enterprise,
