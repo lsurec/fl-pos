a035af5b32acdee72b5e6104062aa12d2653ba0c
Robert Morales <lsurec@demosoft.com.gt>
2024-06-24 14:08:32 -0600

    "Added loading state and data loading functionality to LocationsViewModel and LocationsView"

--- lib/displays/restaurant/view_models/locations_view_model.dart
+++ lib/displays/restaurant/view_models/locations_view_model.dart
@@ -1,6 +1,71 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_post_printer_example/displays/restaurant/models/models.dart';
+import 'package:flutter_post_printer_example/displays/restaurant/services/restaurant_service.dart';
+import 'package:flutter_post_printer_example/displays/shr_local_config/view_models/view_models.dart';
+import 'package:flutter_post_printer_example/models/models.dart';
+import 'package:flutter_post_printer_example/services/services.dart';
+import 'package:flutter_post_printer_example/view_models/view_models.dart';
+import 'package:provider/provider.dart';
 
 class LocationsViewModel extends ChangeNotifier {
+  //manejar flujo del procesp
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  set isLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
   final List<LocationsModel> locations = [];
+
+  Future<void> loadData(BuildContext context) async {
+    final vmLogin = Provider.of<LoginViewModel>(
+      context,
+      listen: false,
+    );
+    final vmLocal = Provider.of<LocalSettingsViewModel>(
+      context,
+      listen: false,
+    );
+
+    final vmMenu = Provider.of<MenuViewModel>(
+      context,
+      listen: false,
+    );
+
+    final int empresa = vmLocal.selectedEmpresa!.empresa;
+    final int estacion = vmLocal.selectedEstacion!.estacionTrabajo;
+    final String user = vmLogin.user;
+    final String token = vmLogin.token;
+    final int tipoDocumento = vmMenu.documento!;
+
+    isLoading = true;
+
+    RestaurantService restaurantService = RestaurantService();
+
+    final ApiResModel resLocations = await restaurantService.getLocations(
+      tipoDocumento,
+      empresa,
+      estacion,
+      "1", //TODO:Preguntar -serie
+      user,
+      token,
+    );
+
+    if (!resLocations.succes) {
+      isLoading = false;
+      NotificationService.showErrorView(context, resLocations);
+      return;
+    }
+
+    final List<LocationsModel> locationRes = resLocations.response;
+
+    locations.clear();
+    locations.addAll(locationRes);
+
+    isLoading = false;
+  }
 }
--- lib/displays/restaurant/views/locations_view.dart
+++ lib/displays/restaurant/views/locations_view.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_post_printer_example/displays/restaurant/view_models/view_models.dart';
+import 'package:flutter_post_printer_example/shared_preferences/preferences.dart';
+import 'package:flutter_post_printer_example/themes/app_theme.dart';
+import 'package:flutter_post_printer_example/utilities/styles_utilities.dart';
+import 'package:flutter_post_printer_example/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LocationsView extends StatelessWidget {
   const LocationsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('LocationsView'),
-      ),
+    final vm = Provider.of<LocationsViewModel>(context);
+
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text("Ubicaciones"), //TODO:TRanslate
+          ),
+          body: RefreshIndicator(
+            onRefresh: () => vm.loadData(context),
+            child: ListView(
+              children: [],
+            ),
+          ),
+        ),
+        if (vm.isLoading)
+          ModalBarrier(
+            dismissible: false,
+            // color: Colors.black.withOpacity(0.3),
+            color: AppTheme.color(
+              context,
+              Styles.loading,
+              Preferences.idTheme,
+            ),
+          ),
+        if (vm.isLoading) const LoadWidget(),
+      ],
     );
   }
 }
