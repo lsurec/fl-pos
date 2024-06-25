42b477534f7dce669c609bac13bb3fd20e72dcd0
Robert Morales <lsurec@demosoft.com.gt>
2024-06-25 16:45:27 -0600

    Added `getAccountPin` method to `RestaurantService` class, modified `PinView` widget, and exported `AccountPinModel` from `models.dart`.

--- lib/displays/restaurant/views/pin_view.dart
+++ lib/displays/restaurant/views/pin_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_post_printer_example/displays/restaurant/view_models/view_models.dart';
+import 'package:flutter_post_printer_example/shared_preferences/preferences.dart';
+import 'package:flutter_post_printer_example/themes/app_theme.dart';
+import 'package:flutter_post_printer_example/utilities/styles_utilities.dart';
 import 'package:flutter_post_printer_example/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
@@ -10,88 +13,94 @@ class PinView extends StatelessWidget {
   Widget build(BuildContext context) {
     final vm = Provider.of<PinViewModel>(context);
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 150),
-              const Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Text(
-                  "Mesero", //TODO:Translate
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              CardWidget(
-                width: double.infinity,
-                raidus: 18,
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Form(
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        key: vm.formKey,
-                        child: TextFormField(
-                            decoration: const InputDecoration(
-                              hintText: 'Pin',
-                              labelText: 'Pin',
-                            ),
-                            onChanged: (value) => {
-                                  vm.pinMesero = value,
-                                },
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Campo requerido.';
-                              }
-                              return null;
-                            },
-                            obscureText: true),
+    return Stack(
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 150),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Mesero", //TODO:Translate
+                      style: AppTheme.style(
+                        context,
+                        Styles.title,
+                        Preferences.idTheme,
                       ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: ElevatedButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: const SizedBox(
-                                width: double.infinity,
-                                child: Center(
-                                  child: Text("Cancelar"),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: ElevatedButton(
-                              onPressed: () => vm.validatePin(),
-                              child: const SizedBox(
-                                width: double.infinity,
-                                child: Center(
-                                  child: Text("Aceptar"),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
                   ),
-                ),
+                  CardWidget(
+                    width: double.infinity,
+                    raidus: 18,
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Form(
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            key: vm.formKey,
+                            //TODO: Translate elemnts
+                            child: TextFormField(
+                                decoration: const InputDecoration(
+                                  hintText: 'Pin',
+                                  labelText: 'Pin',
+                                ),
+                                onChanged: (value) => {
+                                      vm.pinMesero = value,
+                                    },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Campo requerido.';
+                                  }
+                                  return null;
+                                },
+                                obscureText: true),
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  child: const SizedBox(
+                                    width: double.infinity,
+                                    child: Center(
+                                      child: Text("Cancelar"), //TODO:Translate
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () => vm.validatePin(),
+                                  child: const SizedBox(
+                                    width: double.infinity,
+                                    child: Center(
+                                      child: Text("Aceptar"), //TODO:Translate
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
--- lib/displays/shr_local_config/models/models.dart
+++ lib/displays/shr_local_config/models/models.dart
@@ -1,2 +1,3 @@
+export 'package:flutter_post_printer_example/displays/shr_local_config/models/account_pin_model.dart';
 export 'package:flutter_post_printer_example/displays/shr_local_config/models/empresa_model.dart';
 export 'package:flutter_post_printer_example/displays/shr_local_config/models/estacion_model.dart';
