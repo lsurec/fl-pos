d123ebcfdb2444508d32b899aaece9ce98c2e5ac
Robert Morales <lsurec@demosoft.com.gt>
2024-06-26 15:42:11 -0600

    "Added export for ClassificationModel and implemented getClassifications method in RestaurantService"

--- lib/displays/restaurant/models/models.dart
+++ lib/displays/restaurant/models/models.dart
@@ -1,3 +1,4 @@
+export 'package:flutter_post_printer_example/displays/restaurant/models/classification_model.dart';
 export 'package:flutter_post_printer_example/displays/restaurant/models/order_model.dart';
 export 'package:flutter_post_printer_example/displays/restaurant/models/locations_model.dart';
 export 'package:flutter_post_printer_example/displays/restaurant/models/table_model.dart';
--- lib/displays/restaurant/services/restaurant_service.dart
+++ lib/displays/restaurant/services/restaurant_service.dart
@@ -9,6 +9,73 @@ import 'package:http/http.dart' as http;
 class RestaurantService {
   final String _baseUrl = Preferences.urlApi;
 
+  Future<ApiResModel> getClassifications(
+    int typeDoc,
+    int enterprise,
+    int station,
+    String series,
+    String user,
+    String token,
+  ) async {
+    Uri url = Uri.parse("${_baseUrl}Restaurant/classifications");
+    try {
+      //url completa
+
+      //configuracion del api
+      final response = await http.get(
+        url,
+        headers: {
+          "Authorization": "bearer $token",
+          "typeDoc": "$typeDoc",
+          "enterprise": "$enterprise",
+          "station": "$station",
+          "series": series,
+          "user": user,
+          "token": token,
+        },
+      );
+
+      ResponseModel res = ResponseModel.fromMap(jsonDecode(response.body));
+
+      //si el api no responde
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        return ApiResModel(
+          url: url.toString(),
+          succes: false,
+          response: res.data,
+          storeProcedure: res.storeProcedure,
+        );
+      }
+
+      //Empresas disponuibles
+      List<ClassificationModel> classifications = [];
+
+      //recorrer lista api Y  agregar a lista local
+      for (var item in res.data) {
+        //Tipar a map
+        final responseFinally = ClassificationModel.fromMap(item);
+        //agregar item a la lista
+        classifications.add(responseFinally);
+      }
+
+      //Respuesta corerecta
+      return ApiResModel(
+        url: url.toString(),
+        succes: true,
+        response: classifications,
+        storeProcedure: null,
+      );
+    } catch (e) {
+      //respuesta incorrecta
+      return ApiResModel(
+        url: url.toString(),
+        succes: false,
+        response: e.toString(),
+        storeProcedure: null,
+      );
+    }
+  }
+
   Future<ApiResModel> getAccountPin(
     String token,
     int enterprice,
