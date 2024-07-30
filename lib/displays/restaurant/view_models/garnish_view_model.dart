b1cffdaec334bcb1a1738b1d15d6daa4832fcd28
Robert Morales <lsurec@demosoft.com.gt>
2024-07-30 15:28:09 -0600

    "Added imports and implemented loadGarnish method in GarnishViewModel"

--- lib/displays/restaurant/view_models/garnish_view_model.dart
+++ lib/displays/restaurant/view_models/garnish_view_model.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_post_printer_example/displays/restaurant/services/restaurant_service.dart';
+import 'package:flutter_post_printer_example/displays/tareas/models/models.dart';
+import 'package:flutter_post_printer_example/view_models/view_models.dart';
+import 'package:provider/provider.dart';
 
-class GarnishViewModel extends ChangeNotifier {}
+class GarnishViewModel extends ChangeNotifier {
+  Future<ApiResModel> loadGarnish(
+    BuildContext context,
+    int product,
+    int um,
+  ) async {
+    final vmLogin = Provider.of<LoginViewModel>(
+      context,
+      listen: false,
+    );
+
+    final String user = vmLogin.user;
+    final String token = vmLogin.token;
+
+    final RestaurantService restaurantService = RestaurantService();
+
+    final ApiResModel apiResModel = await restaurantService.getGarnish(
+      product,
+      um,
+      user,
+      token,
+    );
+
+    return apiResModel;
+  }
+}
