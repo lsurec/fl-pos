12805bd3bde18919c5f92c6e21a52451add24b8f
Karla Catu <kcatu@demosoft.com.gt>
2024-09-17 09:54:30 -0600

    correccion de error que se daba al navegar al display

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -151,7 +151,7 @@ class _MyDrawer extends StatelessWidget {
                       ? menuVM.navigateDisplay(
                           context,
                           itemMenu.route,
-                          int.parse(itemMenu.display?.tipoDocumento ?? 0),
+                          int.parse(itemMenu.display?.tipoDocumento ?? "0"),
                           itemMenu.display!.name,
                           itemMenu.display!.desTipoDocumento,
                         ) //Mostar contenido
